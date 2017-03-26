7f94df477ccbc6e3066bb0c07970de18b8a1e7a0
Poonam Mishra <poonammishra24398@gmail.com>
2017-03-26 16:26:57 +0530

    Added the initial files for the revceiver's work.

---
+++ Settings.java
@@ -0,0 +1,10 @@
+package piper;
+
+import java.io.*;
+
+class Settings
+{   final static String packetSendFile = "temp_send" + File.separator + "pack";
+    final static String packetReceiveFile = "temp_receive" + File.separator + "pack";
+    final static int noOfPackets = 10;
+    final static int port = 9999;
+}
